7b5bb3d05b79f57ac04384769ce45e8a4029c768
Thomas Anderson <tomp.45a@gmail.com>
2022-05-16 19:55:57 -0500

    stuff

--- Assets/HostileNPC.cs
+++ Assets/HostileNPC.cs
@@ -10,10 +10,11 @@ public class HostileNPC : MonoBehaviour
     public PlayerMovement player;
     public float hitPoints;
     public float movementSpeed;
+    public float maxMovementSpeed;
     public float movementTimer;
     public float buffMultiplier;
     public float popForce;
-    public float jumpForce;
+    public float jumpHeight;
     public float attackRate;
     public float coolDown;
     public bool gettingReadyToMove = false;
@@ -33,7 +34,7 @@ public class HostileNPC : MonoBehaviour
         player = Object.FindObjectOfType<PlayerMovement>();
         rigidBody = GetComponent<Rigidbody>();
         distanceToGround = myCollider.bounds.extents.y;
-        rigidBody.AddForce(transform.up * popForce);
+        //rigidBody.AddForce(transform.up * popForce);
     }
 
     private void Update()
@@ -58,10 +59,16 @@ public class HostileNPC : MonoBehaviour
             
         }
 
-       /* if (inPool == false && gettingReadyToMove == false)
+        if (rigidBody.velocity.magnitude > maxMovementSpeed)
         {
-            //StartCoroutine(RandomMovement());
-        }*/
+            rigidBody.velocity = rigidBody.velocity.normalized * maxMovementSpeed;
+        }
+
+
+        /* if (inPool == false && gettingReadyToMove == false)
+         {
+             //StartCoroutine(RandomMovement());
+         }*/
 
         if (hitPoints <= 0)
         {
@@ -84,36 +91,52 @@ public class HostileNPC : MonoBehaviour
         //Debug.Log("player.gameObject.gameObject.transform.position: " + player.transform.position);
         Vector3 attackPoint = new Vector3(player.gameObject.gameObject.transform.position.x - transform.position.x, transform.position.y, player.gameObject.gameObject.transform.position.z - transform.position.z);
         //attackPoint = player.transform.position - attackPoint;
+        transform.LookAt(attackPoint);
         StartCoroutine(DoAttack(attackPoint));
     }
 
     IEnumerator DoAttack(Vector3 point)
     {
-        float duration = attackRate/3;
-        float normalizedTime = 0;
-        Vector3 positionOne = new Vector3(transform.position.x, transform.position.y, transform.position.z);
         inAttack = true;
-        transform.LookAt(point);
-        rigidBody.AddForce(transform.up * jumpForce);
+
+        float duration = attackRate/8;
+        float normalizedTime = 0;
+        float elapsedTime = 0;
+        //Vector3 positionOne = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        
+        //rigidBody.AddForce(transform.up * jumpForce);
         while (normalizedTime <= 1f)
         {
             normalizedTime += Time.deltaTime / duration;
-            rigidBody.MovePosition(transform.position + positionOne * Time.deltaTime * movementSpeed);
-            positionOne.y += normalizedTime * 2;
+            //if (normalizedTime > .5f) {
+            rigidBody.MovePosition(transform.position + new Vector3(0, Mathf.Lerp(transform.position.y, transform.position.y + jumpHeight, elapsedTime / duration), 0) * Time.deltaTime * movementSpeed);
+            elapsedTime += Time.deltaTime;
+            //}
+            //positionOne.y += normalizedTime * 10;
             yield return null;
         }
         StartCoroutine(DoMove(point));
     }
     IEnumerator DoMove(Vector3 point)
     {
-        float duration = attackRate/3;
+        float duration = attackRate;
         float normalizedTime = 0;
-        transform.LookAt(point);
-        rigidBody.AddForce(transform.up * jumpForce);
+        Vector3 transition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        //float lerpingX = 0;
+        //float lerpingY = 0;
+        float elapsedTime = 0;
+        //float elapsedTime = 0;
+        //transform.LookAt(point);
+        //rigidBody.AddForce(transform.up * jumpForce);
         while (normalizedTime <= 1f)
         {
             normalizedTime += Time.deltaTime / duration;
-            rigidBody.MovePosition(transform.position + point * Time.deltaTime * movementSpeed);
+            transition.x = Mathf.Lerp(transform.position.x, player.gameObject.gameObject.transform.position.x, elapsedTime / duration);
+            transition.z = Mathf.Lerp(transform.position.z, player.gameObject.gameObject.transform.position.z, elapsedTime / duration);
+            rigidBody.MovePosition(transition * Time.deltaTime * movementSpeed);
+            //rigidBody.MovePosition(transform.position + new Vector3(transform.position.x, Mathf.Lerp(transform.position.y, point.y, elapsedTime / duration), transform.position.z) * Time.deltaTime);
+            //elapsedTime += Time.deltaTime;
+            elapsedTime += Time.deltaTime;
             yield return null;
         }
         StartCoroutine(CoolDown());
